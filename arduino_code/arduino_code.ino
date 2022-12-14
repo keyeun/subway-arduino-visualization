16464d077a425838b6fc6f2a1da23cfc79bd03a7
KeyEun Lee <kieunp@snu.ac.kr>
2022-12-15 08:21:19 +0900

    change arduino

--- arduino_code/arduino_code.ino
+++ arduino_code/arduino_code.ino
@@ -41,18 +41,18 @@ void loop() {
     int blue = 0;
 
     if (status == 1) {
-      red = 0;
-      green = 255;
-      blue = 0;
+      red = 28;
+      green = 153;
+      blue = 247;
 
     } else if (status == 2) {
       red = 255;
-      green = 255;
-      blue = 0;
+      green = 105;
+      blue = 46;
     } else if (status == 3) {
-      red = 255;
-      green = 0;
-      blue = 0;
+      red = 244;
+      green = 39;
+      blue = 39;
     }
 
     strip.setPixelColor(index, strip.Color(red, green, blue));
@@ -66,67 +66,6 @@ void loop() {
     check = 0;
     strip.clear();
   }
-  /*
-  delay(1);
-  if (Serial.available() > 0) {
-
-    int index = Serial.parseInt();
-    int status = Serial.parseInt();
-    //String a = Serial.readString();
-    //Serial.println(a);
-    //int b= a.toInt();
-
-    int red = 0;
-    int green = 0;
-    int blue = 0;
-
-    if (status == 1) {
-      red = 0;
-      green = 255;
-      blue = 0;
-
-    } else if (status == 2) {
-      red = 255;
-      green = 255;
-      blue = 0;
-    } else if (status == 3) {
-      red = 255;
-      green = 0;
-      blue = 0;
-    }
-
-    strip.setPixelColor(index, strip.Color(red, green, blue));
-    strip.show();
-
-    //strip.setPixelColor(0, strip.Color(0, 255, 0));
-    // Iterate over the array of colors
-    /*
-      for (int i = 1; i < LED_PIN; i++) {
-        // Get the current color as a string
-        // String color = doc[i];
-        //String a = Serial.readString();
-        //Serial.println(a);
-        //int color= a.toInt();
-        // Check if the current color is "red"
-        if (color == 1) {
-          // Set the Neopixel LED to red
-          strip.setPixelColor(i, strip.Color(255, 0, 0));
-          strip.show();
-        }
-
-        // Check if the current color is "green"
-        else if (color == 2) {
-          // Set the Neopixel LED to green
-          strip.setPixelColor(i, strip.Color(0, 255, 0));
-          strip.show();
-        }
-        else if (color == 3){
-          // Set the Neopixel LED to yellow
-          strip.setPixelColor(i, strip.Color(255, 255, 0));
-          strip.show();
-        }
-      }
-  }*/
 }
 
 void serialEvent() {
